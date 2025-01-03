33321cd83cd7cf30d4901774c4fed7d0a8b0c23f
haberocean <haberocean@gmail.com>
2025-01-03 18:37:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from
+          Learn React from India
         </a>
       </header>
     </div>
